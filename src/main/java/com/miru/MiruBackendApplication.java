26731698a717d03071732fc8c476e3c3ccf9fd61
Chanwoo1124 <lgw22774@gmail.com>
2026-02-04 23:21:11 +0900

    Project Init (Spring Boot 3.x)

---
+++ src/main/java/com/miru/MiruBackendApplication.java
@@ -0,0 +1,13 @@
+package com.miru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiruBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiruBackendApplication.class, args);
+	}
+
+}
