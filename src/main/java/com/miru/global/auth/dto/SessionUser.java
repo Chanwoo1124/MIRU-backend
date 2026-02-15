237d5cc327f697b0e229e3046d832f741bd226cf
min-shy03 <alstngusqkqh@gmail.com>
2026-02-15 10:03:07 +0900

    세션 유저 dto id 필드 추가

--- src/main/java/com/miru/global/auth/dto/SessionUser.java
+++ src/main/java/com/miru/global/auth/dto/SessionUser.java
@@ -7,11 +7,13 @@ import java.io.Serializable;
 
 @Getter
 public class SessionUser implements Serializable { // ★ implements Serializable 필수
+    private Long id;
     private String nickname;
     private String role;
     private String status;
 
     public SessionUser(User user) {
+        this.id = user.getId();
         this.nickname = user.getNickname();
         this.role = user.getRole().getKey();
         this.status = user.getStatus().name();
